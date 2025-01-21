393b4782e1e94e87e10789ba3ccc191a35f5c419
Tony <57773117@qq.com>
2025-01-22 05:31:14 +0800

    优化代码

--- src/Annotation/RateLimiter.php
+++ src/Annotation/RateLimiter.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * This file is part of T2-Engine.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    Tony<dev@t2engine.cn>
+ * @copyright Tony<dev@t2engine.cn>
+ * @link      https://www.t2engine.cn/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types=1);
 
 namespace T2\RateLimiter\Annotation;
 
