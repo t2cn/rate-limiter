8046dbcb61cc98b3e7700a87ff3923864954b79d
Tony <57773117@qq.com>
2025-01-22 05:28:32 +0800

    Install.php 优化代码

--- src/Install.php
+++ src/Install.php
@@ -22,28 +22,22 @@ class Install
      */
     public static function install(): void
     {
-        // 检查本地项目
         if (!self::isFrameworkInstalled(self::VALID_ENGINE_NAMES)) {
             echo "The 't2cn/framework' package is not installed. Installation aborted.\n";
             return;
         }
-
-        // 循环遍历定义要操作的文件
         foreach (static::$pathRelation as $source => $dest) {
-            $sourcePath = __DIR__ . "/$source"; // 源文件路径
-            $targetPath = base_path() . "/$dest"; // 目标文件路径
+            $sourcePath = __DIR__ . "/$source";
+            $targetPath = base_path() . "/$dest";
             // 拷贝文件
             if (!copy($sourcePath, $targetPath)) {
                 echo "Failed to copy directory: $sourcePath to $targetPath\n";
             }
         }
-
-        // 更新 bootstrap.php 文件内容
-        $bootstrapFilePath = base_path() . "/config/bootstrap.php"; // "/Users/dev/Develop.localized/scbtl/engine-multiple/config/bootstrap.php"
-        static::addToArray($bootstrapFilePath, 'T2\\RateLimiter\\Bootstrap::class');
-        // 更新 middleware.php 文件内容
-        $middlewareFilePath = base_path() . "/config/middleware.php"; // "/Users/dev/Develop.localized/scbtl/engine-multiple/config/middleware.php"
-        static::addToArray($middlewareFilePath, 'T2\\RateLimiter\\Limiter::class');
+        $bootstrapFilePath = base_path() . "/config/bootstrap.php";
+        static::addToArray($bootstrapFilePath, 'T2\RateLimiter\Bootstrap::class');
+        $middlewareFilePath = base_path() . "/config/middleware.php";
+        static::addToArray($middlewareFilePath, 'T2\RateLimiter\Limiter::class');
     }
 
     /**
@@ -53,82 +47,61 @@ class Install
      */
     protected static function addToArray(string $filePath, string $newItem): void
     {
-        // 检查文件是否存在
         if (!file_exists($filePath)) {
             echo "File not found: $filePath\n";
             return;
         }
-
-        // 读取文件内容
         $fileContent = file_get_contents($filePath);
         if (!$fileContent) {
             echo "Failed to read file: $filePath\n";
             return;
         }
-
-        // 从文件中提取数组内容
         if (!preg_match('/return\s*\[(.*?)\];/s', $fileContent, $matches)) {
             echo "No return array found in file: $filePath\n";
             return;
         }
-        // 获取数组内容并去除空格、换行符等
         $arrayContent = preg_replace('/\s+/', '', $matches[1]);
+        if (str_contains($arrayContent, $newItem)) {
+            echo "Item already exists in array: $newItem\n";
+            return;
+        }
         switch ($newItem) {
             case 'T2\\RateLimiter\\Bootstrap::class':
             case 'T2\RateLimiter\Bootstrap::class':
-                // 检查要添加的内容是否已经存在
-                if (str_contains($arrayContent, $newItem)) {
-                    echo "Item already exists in array: $newItem\n";
-                    return;
-                }
-                // 格式化数组内容，确保逗号后有空格
-                $arrayContent = preg_replace('/,(?!$)(?=\S)/', ', ', $arrayContent);
-                // 去掉末尾的逗号和空格
-                $arrayContent = rtrim($arrayContent, ', ');
-                // 如果数组内容已经有项，添加逗号和空格，再追加新项
-                $arrayContent .= ($arrayContent ? ', ' : '') . $newItem;
+                $arrayContent = preg_replace('/,(?!$)(?=\S)/', ',', $arrayContent);
+                $arrayContent = rtrim($arrayContent, ',');
+                $arrayContent .= ($arrayContent ? ',' : '') . $newItem;
                 break;
             case 'T2\\RateLimiter\\Limiter::class':
             case 'T2\RateLimiter\Limiter::class':
                 $oArrayContent = $arrayContent;
                 if (!str_contains($arrayContent, '@')) {
-                    // 构造替换后的 '@' 区间字符串
                     $arrayContent = "'@'=>[" . $newItem . '],' . $oArrayContent;
                 } else {
-                    // 查找 '@' 符号的起始位置
-                    $atPosition = strpos($arrayContent, '@');
-                    // 查找 '@' 后面第一个 ']' 的位置
+                    $atPosition           = strpos($arrayContent, '@');
                     $closeBracketPosition = strpos($arrayContent, ']', $atPosition);
                     if ($closeBracketPosition === false) {
                         echo "Closing bracket ']' not found.\n";
                         return;
                     }
-                    // 截取 '@' 前一位到 ']' 后一位的字符串
                     $substring = substr($arrayContent, $atPosition - 1, $closeBracketPosition - $atPosition + 2);
-                    // 使用正则表达式提取中括号内的内容
                     if (!preg_match("/\[(.*?)\]/", $substring, $matches)) {
                         echo "No content found inside brackets.\n";
                         return;
                     }
-                    // 提取中括号内的内容并进行格式化
                     $arrayContent = rtrim(preg_replace('/,\s*/', ',', $matches[1]), ',');
-                    // 添加新的内容到数组中
+                    $arrayContent = rtrim($arrayContent, ',');
                     $arrayContent .= ($arrayContent ? ',' : '') . $newItem;
-                    // 构造替换后的 '@' 区间字符串
-                    $newString = "'@'=>[" . $arrayContent . ']';
-                    // 确定替换的起点和长度
-                    $start  = $atPosition - 1;
-                    $length = $closeBracketPosition - $start + 1;
-                    // 替换原字符串的指定部分
+                    $newString    = "'@'=>[" . $arrayContent . ']';
+                    $start        = $atPosition - 1;
+                    $length       = $closeBracketPosition - $start + 1;
                     $arrayContent = substr_replace($oArrayContent, $newString, $start, $length);
                 }
                 break;
             default:
                 echo "No action was taken\n";
         }
-        // 更新文件内容
         $fileContent = preg_replace('/return\s*\[(.*?)\];/s', "return [$arrayContent];", $fileContent);
-        // 写回更新后的文件内容
         if (file_put_contents($filePath, $fileContent) === false) {
             echo "Failed to update file: $filePath\n";
         }
@@ -141,11 +114,9 @@ class Install
     public static function uninstall(): void
     {
         $file = base_path() . "/config/limiter.php";
-        // 删除配置文件文件
         if (is_file($file) && !unlink($file)) {
             echo "Failed to delete: $file\n";
         }
-        // 移除内容
         $bootstrapFilePath = base_path() . "/config/bootstrap.php";
         static::removeFromArray($bootstrapFilePath, 'T2\\RateLimiter\\Bootstrap::class');
         $middlewareFilePath = base_path() . "/config/middleware.php";
@@ -190,19 +161,16 @@ class Install
     protected static function isFrameworkInstalled(array $haystack): bool
     {
         $composerFilePath = base_path() . '/composer.json';
-        // 没有正确的 composer.json 文件
         if (!file_exists($composerFilePath)) {
             echo "composer.json not found. Cannot verify framework installation.\n";
             return false;
         }
         $composerContent = file_get_contents($composerFilePath);
-        // 读取 composer.json 文件失败
         if ($composerContent === false) {
             echo "Failed to read composer.json.\n";
             return false;
         }
         $composerData = json_decode($composerContent, true);
-        // composer.json 格式无效
         if (json_last_error() !== JSON_ERROR_NONE) {
             echo "Invalid composer.json format.\n";
             return false;
