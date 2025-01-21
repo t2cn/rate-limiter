b62199a7bf1f1cde4082b097c8e27b048d7c2ad7
Tony <57773117@qq.com>
2025-01-21 09:25:15 +0800

    Install.php 优化代码

--- src/Install.php
+++ src/Install.php
@@ -169,12 +169,13 @@ class Install
                 break;
             case 'T2\RateLimiter\Limiter::class':
                 // 使用正则表达式提取方括号中的内容
-                if (preg_match('/\[(.*?)\]/', $arrayContent, $matches)) {
-                    $contentInsideBrackets = $matches[1];  // 提取到的内容，T2\RateLimiter\LimiterA::class
-                    echo "Extracted content: " . $contentInsideBrackets . "\n";  // 输出提取的内容
-                } else {
+                if (!preg_match('/\[(.*?)\]/', $arrayContent, $matches)) {
                     echo "No valid content found inside brackets.\n";
                 }
+                $arrayContent = $matches[1];  // 提取到的内容，T2\RateLimiter\LimiterA::class
+                // 添加新项到数组末尾
+                $arrayContent .= (!str_ends_with($arrayContent, ',') ? ',' : '') . $newItem;
+                var_dump($arrayContent);
                 break;
             default:
                 echo "An error occurred\n";
